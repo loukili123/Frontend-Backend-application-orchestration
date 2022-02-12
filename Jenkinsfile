9c953609d8d8a7931b0f42b2cb39f5f2a5a14fdc
loukili123 <bilal.loukili.pro@gmail.com>
2022-02-12 02:06:34 +0100

    testing

--- Jenkinsfile
+++ Jenkinsfile
@@ -2,6 +2,11 @@ pipeline {
     agent any
 
     stages {
+        stage('Building and running docker images') {
+          steps {
+            bat "docker-compose build -d"
+          }
+        }
 		stage('Testing api') {
           steps {
             bat '''
@@ -14,14 +19,19 @@ pipeline {
 			'''
           }
         }
+		stage('Running Docker image') {
+          steps {
+            bat "docker-compose up -d"
+          }
+        }
 		stage('Creating release') {
           steps {
 
-			withCredentials([usernamePassword(credentialsId: '99838eeb-727b-4918-a323-a392fd8b532a', passwordVariable: 'GIT_PASSWORD', usernameVariable: 'GIT_USERNAME')]) {
-				bat ''' git checkout release 
-						git push 
-					'''
-			}
+				bat'''
+					git checkout dev
+					git push origin dev:release
+				'''
+			
 
           }
         }
--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const session = require('express-session');
+const cors =  require("cors");
 const express = require("express");
 const server = express();
 const port = process.env.SERVER_PORT || 5000;
@@ -15,10 +16,18 @@ server.use(
 );
 
 server.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000/');
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept'
+  );
+
   next();
-  });
+});
+
+
+  //res.header("Access-Control-Allow-Headers", "X-Requested-With");
 
 server.get("/", function(req, res){
 
@@ -30,5 +39,6 @@ server.get("/", function(req, res){
     });
 
 });
+
 
 module.exports = server;
